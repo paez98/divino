c76542b9cf00995d2704a1a6cc20e0814f285f93
paez98 <adrianpz1198@gmail.com>
2024-03-05 06:10:32 -0400

    otro FB

--- Divino/components/float_button.py
+++ Divino/components/float_button.py
@@ -1,5 +1,5 @@
-import reflex as rx 
-
+import reflex as rx
+from .icons import icon
 
 class FloatButton(rx.Component):
     library = 'antd'
@@ -7,10 +7,22 @@ class FloatButton(rx.Component):
     visibilityHeight = '0'
     fontSizeSM = 12,
     fontSizeIcon = 12,
-    controlItemBgHover = 'rgba(120,440, 0, 0.4)'
-    badge = {
-        'dot':'true'
+    _hover = {
+        'color':'rgba(120,440, 0, 0.4)',
+        'background-color':'rgba(120,440, 0, 0.4)'
     }
-    
-    
+    badge = {
+        'dot': 'true'
+    }
+
+
 floatB = FloatButton.create
+
+def float_button()-> rx.Component:
+    return rx.box(
+        rx.link(
+            rx.button(
+                icon('icons/chevron-up-solid.svg')
+            )
+        )
+    )
